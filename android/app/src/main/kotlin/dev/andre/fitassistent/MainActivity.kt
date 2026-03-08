c728a1087e67adac4475330c9865938955e88266
Andrey Solovyh <andrejsolovyh6@gmail.com>
2026-03-09 02:05:57 +0300

    KAN-7 Добавил пример работы через channel с логгированием

--- android/app/src/main/kotlin/dev/andre/fitassistent/MainActivity.kt
+++ android/app/src/main/kotlin/dev/andre/fitassistent/MainActivity.kt
@@ -1,5 +1,43 @@
 package dev.andre.fitassistent
 
+import android.util.Log
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+
+    private var counter = 0
+    private val CHANNEL = "counter"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        Log.d("FLUTTER_NATIVE", "Channel initialized")
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .setMethodCallHandler { call, result ->
+
+                Log.d("FLUTTER_NATIVE", "Method called: ${call.method}")
+
+                when (call.method) {
+
+                    "getCounter" -> {
+                        Log.d("FLUTTER_NATIVE", "getCounter -> $counter")
+                        result.success(counter)
+                    }
+
+                    "incrementCounter" -> {
+                        counter++
+                        Log.d("FLUTTER_NATIVE", "incrementCounter -> $counter")
+                        result.success(counter)
+                    }
+
+                    else -> {
+                        Log.d("FLUTTER_NATIVE", "Method not implemented")
+                        result.notImplemented()
+                    }
+                }
+            }
+    }
+}
